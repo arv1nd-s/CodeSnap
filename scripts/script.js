a8e3df987124e4675bbaad0441299f3263d255c6
Arvind Suthar <arvindsuthar198@gmail.com>
2023-03-26 21:36:45 +0530

    editor added using codemirror

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+CodeMirror(document.querySelector('#my-div'), {
+    lineNumbers: false,
+    scrollbarStyle: null,
+    tabSize: 2,
+    value: 'console.log("Hello, JavaScript");',
+    theme: 'monokai'
+});
